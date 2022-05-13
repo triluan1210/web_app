af1605c4ce130aa89dfa3fcca3d5793184a70ed2
triluan1210 <105469231+triluan1210@users.noreply.github.com>
2022-05-13 19:28:48 +0700

    Update app.py

--- app.py
+++ app.py
@@ -101,7 +101,7 @@ def main():
                     preds = model.predict(a)
                     print(preds)
                     if max(preds[0]) <= 0.80:
-                    st.text("THIS IS NOT A FILE OF LUNG DICOM")
+                        st.text("THIS IS NOT A FILE OF LUNG DICOM")
 
                     else:
                         label = classes[np.argmax(preds)]
