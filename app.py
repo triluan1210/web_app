6c2a44e70904117f446e8468f9e8f262a305ba75
triluan1210 <105469231+triluan1210@users.noreply.github.com>
2022-05-13 18:20:51 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
-import re
-from statistics import mode
+
 import streamlit as st
 import pydicom
 import numpy as np
@@ -10,64 +9,101 @@ from tensorflow import keras
 #pydicom.config.Settings.infer_sq_for_un_vr = True
 
 st.set_option('deprecation.showfileUploaderEncoding', False)
+from PIL import Image
+image = Image.open('D:\LVTN_1\streamlit-template\Capture.PNG')
 
+st.sidebar.image(image,channels="RGB",width =250)
 class WrongFileType(ValueError):
     pass
 
 def main():
     st.title("CLASSIFICATION MEDICAL DICOM")
     st.sidebar.title("Configuration")
+    st.sidebar.text("YOU ONLY CHOSE LUNG IMAGE")
+    mode1 = st.sidebar.radio(
+        "Select type of input",
+        ('Image', 'Dicom'))
+    if mode1 == 'Dicom':
+        dicom_bytes = st.sidebar.file_uploader("Upload file", type=["dcm","dicom"])
+        # Config
+        print(dicom_bytes)
+        classes = ['Aortic enlargement ', 'Covid', 'Opacity', 'Normal']
+        model = keras.models.load_model('D:\LVTN_1\streamlit-template\VGG16_224.h5')
 
-    dicom_bytes = st.sidebar.file_uploader("Upload DICOM file", type=["dcm","dicom"])
+        mode = st.sidebar.radio(
+            "Select input source",
+            ('View Image', 'View information'))
 
-    # Config
-    classes = ['Aortic enlargement ', 'Covid', 'Opacity', 'Normal']
+        if not dicom_bytes:
+            raise st.stop()
 
+        try:
+            dicom_header = pydicom.read_file(dicom_bytes, force=True)
+            image_dicom = dicom_header.pixel_array/4095
+        except:
+            st.write(WrongFileType("Does not appear to be a DICOM file"))
 
-    #model_chose= st.sidebar.radio(
-        #"Model Selection",
-        #('VGG16_224_standard', 'VGG16_224_proposed', 'RES_224_standard','RES_224_proposed'))
+        if mode == 'View Image':
 
+            if st.sidebar.button("Load Image"):
+                st.image(image_dicom,width =400)
 
-    model = keras.models.load_model("RES_224_proposed.h5")
+            if st.sidebar.button("Predicted"):
+                st.image(image_dicom,width =400)
+                #print(image_dicom.shape)
+                my_data2 = cv2.resize(image_dicom, (224, 224))
+                a = my_data2.reshape(-1, 224, 224, 1)
+                # pass the image through the network to obtain our predictions
+                preds = model.predict(a)
+                print(max(preds[0]))
+                if max(preds[0]) <= 0.75:
+                    st.text("THIS IS NOT A FILE OF LUNG DICOM")
 
-    mode = st.sidebar.radio(
-        "Select input source",
-        ('View Image', 'View information'))
+                else:
+                    label = classes[np.argmax(preds)]
+                    st.text("THE RESULT OF DICOM IS: " + label + " WITH ACCURACY IS " + str(max(preds[0])*100) + " %")
 
-    if not dicom_bytes:
-        raise st.stop()
+        if mode == 'View information' and st.sidebar.button("Load Information"):
+            view = dicom_header.__repr__().split("\n")
+            view = "\n".join(view)
+            f"""
+                ```
+                {view}
+                ```
+                """
 
-    try:
-        dicom_header = pydicom.read_file(dicom_bytes, force=True)
+    if mode1 == 'Image':
+        image_bytes = st.sidebar.file_uploader("Upload file", type=["jpg", "png"])
+        # Config
+        classes = ['Covid', 'Normal', 'Pneumonia']
+        model = keras.models.load_model('D:\LVTN_1\streamlit-template\RESNET50_224_proposed.h5')
 
-        image_dicom = dicom_header.pixel_array / 4095
-    except:
-        st.write(WrongFileType("Does not appear to be a DICOM file"))
-
-    if mode == 'View Image':
+        def load_image(img):
+                im = Image.open(img)
+                image = np.array(im)
+                return image
+        image = load_image(image_bytes)
+        image = image/255
+            #print(image.shape)
+            #print(image)
 
         if st.sidebar.button("Load Image"):
-            st.image(image_dicom,width =400)
+                    st.image(image_bytes, width=400)
 
         if st.sidebar.button("Predicted"):
-            st.image(image_dicom,width =400)
-            my_data2 = cv2.resize(image_dicom, (224, 224))
-            a = my_data2.reshape(-1, 224, 224, 1)
-            # pass the image through the network to obtain our predictions
-            preds = model.predict(a)
-            label = classes[np.argmax(preds)]
-            st.text("RESULT : " + label)
-
-    if mode == 'View information' and st.sidebar.button("Load Information"):
-        view = dicom_header.__repr__().split("\n")
-        view = "\n".join(view)
-        f"""
-            ```
-            {view}
-            ```
-            """
-
+                    st.image(image_bytes, width=400)
+                    #print(image_bytes.shape)
+                    my_data2 = cv2.resize(image, (224, 224))
+                    #my_data2 = image_bytes/255
+                    a = my_data2.reshape(-1, 224, 224,3)
+                    # pass the image through the network to obtain our predictions
+                    preds = model.predict(a)
+                    print(preds)
+                    if max(preds[0]) <= 0.94:
+                        st.text("THIS IS NOT A FILE OF LUNG IMAGE")
+                    else:
+                        label = classes[np.argmax(preds)]
+                        st.text("RESULT : " + label)
 
 if __name__ == "__main__":
     try:
