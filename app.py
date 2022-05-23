879795fe8a918e389fb22bb62233bba0b5acb5f8
triluan1210 <105469231+triluan1210@users.noreply.github.com>
2022-05-23 13:07:05 +0700

    Update app.py

--- app.py
+++ app.py
@@ -109,7 +109,7 @@ def main():
                     # pass the image through the network to obtain our predictions
                     preds = model.predict(a)
                     print(preds)
-                    if max(preds[0]) <= 0.5:
+                    if max(preds[0]) <= 98:
                         st.text("THIS IS NOT A FILE OF LUNG IMAGE")
 
                     else:
